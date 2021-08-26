80f2cb6dca3ba096ee7649edf761c5596421b71c
FrankLaurora <francesco.laurora9@gmail.com>
2021-08-26 16:44:08 +0200

    Add information showing in html

--- js/script.js
+++ js/script.js
@@ -9,3 +9,13 @@ lastNameList = lastNameList.sort();
 console.log(lastNameList);
 
 console.log(lastNameList.indexOf(yourLastName) + 1);
+
+document.getElementById("user_position").innerHTML = lastNameList.indexOf(yourLastName) + 1;
+
+for (var i = 0; i < lastNameList.length; i++) {
+    if (i < lastNameList.length - 1) {
+        document.getElementById("list_names").innerHTML += lastNameList[i] + ", ";
+    } else {
+        document.getElementById("list_names").innerHTML += lastNameList[lastNameList.length - 1] + ".";
+    }
+}
